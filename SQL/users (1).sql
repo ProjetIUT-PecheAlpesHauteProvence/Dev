74f3f28a5a84665f0b471abfc586ff10f42aa759
hurryy <yannhurry@hotmail.fr>
2014-11-28 14:35:19 +0100

    Table Users

---
+++ SQL/users (1).sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Ven 28 Novembre 2014 à 14:34
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `croogo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) NOT NULL,
+  `username` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `firstname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `website` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `activation_key` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `birthdate` date NOT NULL,
+  `street` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `zipcode` int(5) NOT NULL,
+  `city` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bio` text COLLATE utf8_unicode_ci,
+  `timezone` varchar(40) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `updated` datetime NOT NULL,
+  `updated_by` int(20) DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `created_by` int(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`id`, `role_id`, `username`, `password`, `name`, `firstname`, `email`, `website`, `activation_key`, `birthdate`, `street`, `zipcode`, `city`, `image`, `bio`, `timezone`, `status`, `updated`, `updated_by`, `created`, `created_by`) VALUES
+(1, 1, 'hurryy', '325c21489fa2de1fd53f6626217965b3ea6cdc0c', 'hurry', 'yann', 'yannhurry@hotmail.fr', '', '454ab3c6d4d04fc9de262f360ea9d578', '0000-00-00', '', 0, '', NULL, NULL, 'Europe/Paris', 1, '2014-11-28 12:27:47', 1, '2014-11-28 09:20:30', NULL);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
